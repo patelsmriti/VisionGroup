94b4974cb983bee520877affb876a0149ea46347
patelsmriti <58173389+patelsmriti@users.noreply.github.com>
2021-08-04 13:51:05 +0530

    Create Main.java
    ist branch

---
+++ Main.java
@@ -0,0 +1,28 @@
+package net.codejava.io;
+ 
+import java.io.FileReader;
+import java.io.IOException;
+ 
+/**
+ * This program demonstrates how to read characters from a text file.
+ * @author www.codejava.net
+ *
+ */
+public class TextFileReadingExample1 {
+ 
+    public static void main(String[] args) {
+        try {
+            FileReader reader = new FileReader("MyFile.txt");
+            int character;
+ 
+            while ((character = reader.read()) != -1) {
+                System.out.print((char) character);
+            }
+            reader.close();
+ 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+ 
+}
