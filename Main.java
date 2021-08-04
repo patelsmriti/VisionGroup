96d94b217892f699733dbab6d6fff56c91fc7ca7
patelsmriti <58173389+patelsmriti@users.noreply.github.com>
2021-08-04 14:28:49 +0530

    Add new feture
    more code

--- Main.java
+++ Main.java
@@ -20,7 +20,8 @@ public class TextFileReadingExample1 {
             }
          System.out.print("Hi");
             reader.close();
- 
+ System.out.println("Object has been deserialized ");
+            
         } catch (IOException e) {
             e.printStackTrace();
         }
